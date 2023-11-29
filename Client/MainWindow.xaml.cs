c81ba5964ba8ad5ecd25465df0bf6aedf08e122b
PingWin <vitya.staritsin@yandex.ru>
2023-11-29 21:03:14 +0300

    1

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -18,60 +18,43 @@ using System.Threading;
 
 namespace Client
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        TcpClient client; 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Accept_Click(object sender, RoutedEventArgs e)
+        private async  void Accept_Click(object sender, RoutedEventArgs e)
         {
-            TcpClient client = new TcpClient();
+            
+            Method("exit123");
+                    
+        }
+
+        
+
+        private  void Method(string str)
+        {
+            client = new TcpClient();
             try
             {
-                client.Connect(IPAddress.Parse("192.168.88.75"), 50000);
+                 client.Connect(IPAddress.Parse("192.168.88.75"), 50000);
 
                 NetworkStream stream = client.GetStream();
 
-                byte[] data = Encoding.Unicode.GetBytes("exit1");
-                byte[] recive =BitConverter.GetBytes(data.Length);
-                stream.Write(recive, 0, recive.Length);
-
+                byte[] data = Encoding.Unicode.GetBytes(str);
                 stream.Write(data, 0, data.Length);
-                Console.WriteLine("Запрос отправлен");
 
-
-                //Thread.Sleep(1000);
-                
-                int count;
-                string message="";
                 byte[] buffer = new byte[256];
                 stream.Read(buffer, 0, buffer.Length);
-                count= BitConverter.ToInt32(buffer, 0);
-                byte[] bytes= new byte[count];
-                stream.Read(bytes, 0, count);
-                textout.Text=Encoding.Unicode.GetString(bytes);
 
-                /*
-                while ((count = stream.Read(buffer, 0, buffer.Length)) != 0)
-                {
-                     message+= Encoding.Unicode.GetString(buffer, 0, count);
-                    Console.WriteLine("GGG");
-                }*/
-               
-                //client.Close();
-                
-                //textout.Text = message;
-
-                //count = stream.Read(buffer, 0, buffer.Length);
-                //message += Encoding.Unicode.GetString(buffer, 0, count);
-                //MessageBox.Show(message);
+                textout.Text = string.Empty;
+                textout.Text = Encoding.Unicode.GetString(buffer);
             }
-            
+
             catch (SocketException se)
             {
                 Console.WriteLine(se);
@@ -80,13 +63,12 @@ namespace Client
             {
                 Console.WriteLine(ex);
             }
-            finally
-            {
-                client.Close();
-            }
+        }
 
-
-            
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Method("exit");
+            client?.Close();
         }
     }
 }
